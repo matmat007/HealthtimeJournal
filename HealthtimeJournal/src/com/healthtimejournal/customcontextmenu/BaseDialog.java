c21d4d29c51072b2c7f342ba1066f9ce13a40c20
matmat007 <matthew_que06@yahoo.com>
2013-08-07 16:30:45 +0800

    commit

--- HealthtimeJournal/src/com/healthtimejournal/customcontextmenu/BaseDialog.java
+++
@@ -1,7 +0,0 @@
-package com.healthtimejournal.customcontextmenu;
-
-import android.content.Context;
-
-public abstract class BaseDialog {
-	public abstract void openDialog(Context context);
-}
--- HealthtimeJournal/src/com/healthtimejournal/model/Event.java
+++
@@ -1,5 +0,0 @@
-package com.healthtimejournal.model;
-
-public class Event {
-
-}
